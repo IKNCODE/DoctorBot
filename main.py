3846b6d390fb7cd857d3034224370814c50df7e9
IKNCODE <ajkaen863@gmail.com>
2024-01-08 20:47:10 +0300

    add mssql connection

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
+from aiogram import Bot, Dispatcher, executor, types
+import pyodbc
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
+server = r"Server=DESKTOP-OC4UNCB;Database=DistrictDat;Trusted_Connection=True;"
 
+TOKEN_API = "6506274690:AAEx5vZAoICHu7u4vSrxtjNCvqxkSUl6Xjs"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+storage = MemoryStorage()
+bot =Bot(token=TOKEN_API)
+dp = Dispatcher(bot, storage=storage)
 
+user = []
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-OC4UNCB;DATABASE=DistrictDat;Trusted_Connection=yes;')
+cursor = conn.cursor()
+cursor.execute("""
+SELECT ФИО FROM Амбулаторная_карта
+""")
+row = cursor.fetchone()
+print(row[0])
 
-# Press the green button in the gutter to run the script.
+@dp.message_handler(commands=["start"])
+async def hi(message : types.Message):
+    conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-OC4UNCB;DATABASE=DistrictDat;Trusted_Connection=yes;')
+    cursor = conn.cursor()
+    cursor.execute("""
+    SELECT ФИО FROM Амбулаторная_карта
+    """)
+    row = cursor.fetchall()
+    for i in range(len(row)):
+        await bot.send_message(text=row[i][0], chat_id=message.from_user.id)
+    conn.close()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    executor.start_polling(dispatcher=dp, skip_updates=True)
